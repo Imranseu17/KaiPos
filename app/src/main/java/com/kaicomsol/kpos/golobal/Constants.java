53428972516f47314600e7505ce62df854ecdabb
anwarseu <anwar@kaicomsol.com>
2019-02-22 18:12:24 +0600

    Customer code added

--- app/src/main/java/com/kaicomsol/kpos/golobal/Constants.java
+++ app/src/main/java/com/kaicomsol/kpos/golobal/Constants.java
@@ -2,23 +2,15 @@ package com.kaicomsol.kpos.golobal;
 
 public class Constants {
 
-    //Local host Development BASE URL
-    //public static String BASE_URL = "http://192.168.1.15:8080";
-
-    //Local Development BASE URL
-    //public static String BASE_URL = "http://103.86.197.138:8181";
-
     //Local Development BASE URL HTTPS
-   public static String BASE_URL = "https://103.86.197.138:8443";
+    //public static String BASE_URL = "https://103.86.197.138:8443";
 
-    //KGDCL Development BASE URL
+    //KGDCL LIVE BASE URL
+    public static String BASE_URL = "https://23.200.31.203:8443";
+
+    //KGDCL host PILOT BASE URL HTTPS
     //public static String BASE_URL = "https://23.101.20.169:8443";
 
-    //Local host Development BASE URL Niab
-    //public static String BASE_URL = "https://192.168.1.11:8443";               
-
-    //Local host Development BASE URL Adnan Vai
-    //public static String BASE_URL = "https://192.168.1.42:8443";
 
     public static final String CONNECT_TO_WIFI = "WIFI";
     public static final String CONNECT_TO_MOBILE = "MOBILE";
